203de4da1ccfc092a123a25d99971dbe4113a5a4
AnchalMishra-2025 <anchalm8390@gmail.com>
2023-12-08 17:29:48 +0530

    Episode-2 Function composition

--- App.js
+++ App.js
@@ -1,8 +1,24 @@
 import React from "react";
 import * as ReactDOM  from "react-dom";
+const number = <span>{100}</span>
 
-const App=()=>{
-    return <h1>Functional Component</h1>
+const Title=()=>{
+    return (
+        <div>
+            <h1>Here we are going to perform Component composition by putting this Component indide App component.</h1>
+            {/* Accessing number react element inside Title component that itself is react element.  */}
+            {number} 
+        </div>
+    );
 }
+const App=()=>{
+    return( <div>
+    <h1>Functional Component</h1>
+    {/* Accessing Title conponent inside Heading component   */}
+    <Title />
+    </div>
+    );
+}
+
 const root1 = ReactDOM.createRoot(document.getElementById("root"));
 root1.render(<App />); 
